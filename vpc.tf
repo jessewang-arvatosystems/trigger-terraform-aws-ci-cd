fc7705d5e2e905838dcbfd2a2c08e40450694bb6
Jesse Wang <jesse.wang@arvatosystems.com>
2022-10-14 16:03:26 -0400

    Add new tag

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,8 @@ resource "aws_vpc" "main" {
   instance_tenancy = "default"
 
   tags = {
-    Creator = "terraform example"
+    Creator = "terraform example",
+    Id = "terraform created vpc"
   }
 }
 
@@ -12,6 +13,7 @@ resource "aws_subnet" "main" {
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Creator = "terraform example"
+    Creator = "terraform example",
+    Id = "terraform created subnet"
   }
 }
