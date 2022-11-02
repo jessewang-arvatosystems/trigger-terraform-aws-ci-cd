bf525a6c962e86bc5ddfa5e1f72f9b2e5cff2456
Jesse Wang <jesse.wang@arvatosystems.com>
2022-11-02 15:11:42 -0400

    Trigger minor change pt2

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "infra-vpc-backend"
+    bucket  = "cdktf-bucket-name"
     encrypt = true
     region  = "us-east-1"
     key     = "terraform_backend/terraform.tfstate"
